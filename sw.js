a916d28378b515fd0d91ad86daba2fe6994906d2
cloudLauncher <91776534+cloudLauncher@users.noreply.github.com>
2023-02-07 23:26:38 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,19 +10,23 @@ let staticFiles = ["cc.js", "manifest.json", "layouts.css", "culture.xml", "fort
     //"https://app.culturecrossover.eu/dice/dado_fx_03.mp3","https://app.culturecrossover.eu/dice/dice-1.jpg"
 ]
 
-const refresh = async function () {
-    lang = '';
+const update = async function () {
     try {
         let keys = await caches.keys();
         await Promise.all(keys.map(key => { return caches.delete(key) }))
+        let cache = await caches.open(cacheName);
+        await cache.addAll(staticFiles)
+
     }
-    catch (err) { client.postMessage({ msg: "refresh fail" }) }
+    catch (err) { clients[0].postMessage({ type: "ERROR", msg: "refresh fail", }) }
 }
 
 self.onmessage = async function (event) {
-    if (event.data.type == 'REFRESH') return refresh();
-    if (lang == event.data.msg) { return; }
-    lang = event.data.msg;
+    if (event.data.type == 'UPDATE') await update();
+    if (event.data.type == 'LANGUAGE') {
+        if (lang == event.data.msg) return;
+        else lang = event.data.msg;
+    }
     try {
         let files = [];
         for (let x of cards) {
@@ -30,22 +34,18 @@ self.onmessage = async function (event) {
                 files.push(`https://app.culturecrossover.eu/wp-json/crossover/${lang}/${x}/${i}`)
             }
         }
-        files.push(`https://culturecrossover.eu/wp-content/uploads/2022/10/Rules_${lang}.pdf`)
+        //files.push(`https://culturecrossover.eu/wp-content/uploads/2022/10/Rules_${lang}.pdf`)
         let cache = await caches.open(cacheName);
         await cache.addAll(files);
-        await cache.addAll(staticFiles)
     }
     catch (err) {
-        console.log('cache failed')
+        console.log(err)
         let clients = await self.clients.matchAll({ type: 'window' });
         if (clients && clients.length >= 1) clients[0].postMessage({ type: "ERROR", msg: "cache fail", });
     }
 }
 
-self.oninstall = function () {
-    skipWaiting();
-    console.log('service worker installed');
-};
+self.oninstall = function () { skipWaiting() };
 
 self.onactivate = function (event) {
     event.waitUntil(async function () {
@@ -63,7 +63,7 @@ const handler = async function (request) {
     if (request.mode == "navigate") res = await caches.match("index.html");
     else res = await caches.match(request)
     if (res) { console.log('return from cache'); return res; }
-    return fetch(request);
+    else return fetch(request);
 }
 
 
